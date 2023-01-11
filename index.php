9b816f63f9d85dfbaf67beb94ba4fde078cd03ae
DanieleAntonaci <danieleanto96@gmail.com>
2023-01-11 12:51:40 +0100

    Creazione helper.php e spostata funzione

--- index.php
+++ index.php
@@ -6,26 +6,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>php-strong-password-generator</title>
     <?php
+        require_once __DIR__ . "/libs/helper.php";
+
         $passwordLength = $_GET['passwordLength'] ?? 0;
-
-        function generateRandomString($passwordLength) {
-
-            // caratteri per la password
-            $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ.,/?><"|"[]{}()';
-
-            // lunghezza stringa caratteri
-            $charactersLength = strlen($characters);
-
-            // stringa di ritorno
-            $randomString = '';
-
-            // ciclo generazioe stringa
-            for ($i = 0; $i < $passwordLength; $i++) {
-                $randomString .= $characters[rand(0, $charactersLength - 1)];
-            }
-            return $randomString;
-        }
-
     ?>
 </head>
 <body>
---
+++ libs/helper.php
@@ -0,0 +1,19 @@
+<?php
+
+function generateRandomString($passwordLength) {
+
+    // caratteri per la password
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ.,/?><"|"[]{}()';
+
+    // lunghezza stringa caratteri
+    $charactersLength = strlen($characters);
+
+    // stringa di ritorno
+    $randomString = '';
+
+    // ciclo generazioe stringa
+    for ($i = 0; $i < $passwordLength; $i++) {
+        $randomString .= $characters[rand(0, $charactersLength - 1)];
+    }
+    return $randomString;
+};
