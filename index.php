dd1dd3bcf3b860626236e220485bb469a5bb989f
DanieleAntonaci <danieleanto96@gmail.com>
2023-01-11 12:39:47 +0100

    Aggiunti commenti funzione

--- index.php
+++ index.php
@@ -9,9 +9,17 @@
         $passwordLength = $_GET['passwordLength'] ?? 0;
 
         function generateRandomString($passwordLength) {
+            
+            // caratteri per la password
             $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+            // lunghezza stringa caratteri
             $charactersLength = strlen($characters);
+
+            // stringa di ritorno
             $randomString = '';
+
+            // ciclo generazioe stringa
             for ($i = 0; $i < $passwordLength; $i++) {
                 $randomString .= $characters[rand(0, $charactersLength - 1)];
             }
