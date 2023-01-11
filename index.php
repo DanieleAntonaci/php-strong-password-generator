71a447307a6b0c9468f8fca0fb21eb7f2ca34ddd
DanieleAntonaci <danieleanto96@gmail.com>
2023-01-11 12:38:02 +0100

    Crezione password random

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php-strong-password-generator</title>
+    <?php
+        $passwordLength = $_GET['passwordLength'] ?? 0;
+
+        function generateRandomString($passwordLength) {
+            $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+            $charactersLength = strlen($characters);
+            $randomString = '';
+            for ($i = 0; $i < $passwordLength; $i++) {
+                $randomString .= $characters[rand(0, $charactersLength - 1)];
+            }
+            return $randomString;
+        }
+
+    ?>
+</head>
+<body>
+    <form method="get">
+        <input type="number" name="passwordLength">
+        <input type="submit" value="number">
+    </form>
+
+    <?php
+        echo  generateRandomString($passwordLength)
+    ?>
+</body>
+</html>
